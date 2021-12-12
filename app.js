bf1e985c0563ca68de9daf2923ceffa5defe8081
Alex <alekcervice@mail.ru>
2021-12-12 22:58:32 +0300

    add personalMovieDB

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-let sayHello = function () {
-    console.log("sayHello")
-}
-sayHello();
+let numberOfFilms = ("сколько фильмов выы посмотрели?", "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("один из последних просмотренных фильмов", ""),
+      b = prompt("на сколько оцените его", ""),
+      c = prompt("один из последних просмотренных фильмов", "")
+      d = prompt("на сколько оцените его", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
